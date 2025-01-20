f1c5ad871487ca48dac27c84ee809bbc20da1cb9
jkk290 <158115736+jkk290@users.noreply.github.com>
2025-01-19 15:03:01 -1000

    Add mouseover coloring effect
    Added the functionality of coloring the page with
    mouse cursor.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const container = document.querySelector('#container');
 
-
 function createDiv() {
-    const squareDivs = document.createElement('div');
-    container.append(squareDivs);
+    const squareDiv = document.createElement('div');
+    squareDiv.addEventListener('mouseenter', (event) => {
+        event.target.style.backgroundColor = 'green';
+    });
+    container.append(squareDiv);
 }
 
 for (let i = 0; i < 256; i++) {
     createDiv();
 }
+
